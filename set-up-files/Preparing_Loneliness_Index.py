3eaa5bc5dcdfac4780dcb3ed3cb6e9a37802e8b9
MilesHewitt <68699731+MilesHewitt@users.noreply.github.com>
2020-12-08 19:44:19 +0000

    Update Preparing_Loneliness_Index.py

--- set-up-files/Preparing_Loneliness_Index.py
+++ set-up-files/Preparing_Loneliness_Index.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
+from scipy import stats
+from pylab import savefig
+%matplotlib inline
 
