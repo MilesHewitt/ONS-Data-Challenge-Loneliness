485b7997a43e65f01153d7fe0438d9f7cf21632a
MilesHewitt <68699731+MilesHewitt@users.noreply.github.com>
2020-12-09 12:32:28 +0000

    Update Preparing_Loneliness_Index.py

--- set-up-files/Preparing_Loneliness_Index.py
+++ set-up-files/Preparing_Loneliness_Index.py
@@ -2,14 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
-import seaborn as sns
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-from statsmodels.sandbox.regression.predstd import wls_prediction_std
-from scipy import stats
-from pylab import savefig
 %matplotlib inline
 
-#Read prepared dataset
-master = pd.read_csv("Master_Dataset.csv")
-
+lonely = pd.read_csv('msoa_loneliness.csv')
+lonely['msoa11nm'] = lonely['msoa11nm'].str[:-4]
+lonely_count = lonely.groupby(['msoa11nm']).count()
+lonely_main = lonely.groupby(['msoa11nm']).sum()
+lonely_main['Average'] = lonely_main['loneills_2018']/lonely_count['objectid']
+lonely_main.to_csv('loneliness_average.csv', index=True)
