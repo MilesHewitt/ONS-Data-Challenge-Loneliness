f434b92c92ecb81c322209e3436f3f210702e48b
MilesHewitt <68699731+MilesHewitt@users.noreply.github.com>
2020-12-09 12:47:02 +0000

    Update Preparing_Loneliness_Index.py

--- set-up-files/Preparing_Loneliness_Index.py
+++ set-up-files/Preparing_Loneliness_Index.py
@@ -4,9 +4,14 @@ import numpy as np
 import matplotlib as mpl
 %matplotlib inline
 
+#Read and manipulate data to be used for next code
 lonely = pd.read_csv('msoa_loneliness.csv')
 lonely['msoa11nm'] = lonely['msoa11nm'].str[:-4]
+
+#Count and sum loneliness data to get average loneliness for local authority
 lonely_count = lonely.groupby(['msoa11nm']).count()
 lonely_main = lonely.groupby(['msoa11nm']).sum()
 lonely_main['Average'] = lonely_main['loneills_2018']/lonely_count['objectid']
+
+#Save the file to be copied over to master dataset
 lonely_main.to_csv('loneliness_average.csv', index=True)
